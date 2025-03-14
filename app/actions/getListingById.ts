f16fc1632ad0dba9002a6dae16dbcb1592325ac4
Suradit <luosuradit@gmail.com>
2025-03-14 16:34:52 +0700

    fixing deployment

--- app/actions/getListingById.ts
+++ app/actions/getListingById.ts
@@ -1,37 +1,31 @@
 import prisma from "@/app/libs/prismadb";
+import { SafeListing } from "@/app/types";
 
-interface IParams {
-   listingId?: string;
-}
+const getListingById = async (params: { listingId?: string }): Promise<SafeListing | null> => {
+  const { listingId } = params;
+  if (!listingId) {
+    return null;
+  }
 
-export default async function getListingById(params: IParams) {
-   try {
-      const { listingId } = params;
+  const listing = await prisma.listing.findUnique({
+    where: { id: listingId },
+    include: { user: true },
+  });
 
-      const listing = await prisma.listing.findUnique({
-         where: {
-            id: listingId,
-         },
-         include: {
-            user: true,
-         },
-      });
+  if (!listing) {
+    return null;
+  }
 
-      if (!listing) {
-         return null;
-      }
+  return {
+    ...listing,
+    createdAt: listing.createdAt.toISOString(),
+    user: {
+      ...listing.user,
+      createdAt: listing.user.createdAt.toISOString(),
+      updatedAt: listing.user.updatedAt.toISOString(),
+      emailVerified: listing.user.emailVerified?.toISOString() || null,
+    },
+  };
+};
 
-      return {
-         ...listing,
-         createdAt: listing.createdAt.toISOString(),
-         user: {
-            ...listing.user,
-            createdAt: listing.user.createdAt.toISOString(),
-            updatedAt: listing.user.updatedAt.toDateString(),
-            emailVerified: listing.user.emailVerified?.toISOString || null,
-         },
-      };
-   } catch (error: any) {
-      throw new Error(error);
-   }
-}
+export default getListingById;
